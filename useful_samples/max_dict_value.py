28bdaf1683152905b566d6e4764cf051456eee5c
DevCTx <contact.devct@gmail.com>
2023-06-08 16:30:38 -0700

    Add get key on max dict values solutions and timings

--- useful_samples/max_dict_value.py
+++ useful_samples/max_dict_value.py
@@ -12,4 +12,5 @@ print( max(d.items(), key=lambda x: x[1])[0], time.time()-start )
 start = time.time()
 print( max(d, key=d.get) , time.time()-start )
 
-
+# v134419 0.21000051498413086
+# v134419 0.23203754425048828   # similar
